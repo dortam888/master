5fff617ffbc9010e261b350d6ed87365889de207
dor.tambour <dortam888@gmail.com>
2019-07-07 15:40:20 +0300

    modified:   ws1.c 	deleted:    WS1.c

--- c/ws1.c
+++ c/ws1.c
@@ -1,92 +1,132 @@
-    #include <stdio.h>
-    #include <math.h>
-    float Pow(int exponent);
-    int Flip(int number);
-    void Swap(int var1, int var2);
-    void TestPow(int exponent);
-    
-    int main()
-    {
-    return 0;
-    }
-    
-    
-    float Pow(int exponent)
-    {
-        
-        float base = 10;
-        float result = 1;
-        
-        if(exponent < 0)
-        {
-            base = 1.0 / base;
-            exponent = -exponent;
-        }
-            
-        for(int i = 0; i < exponent; i++)
-        {
-            result *= base;
-        }
-        
-        return result;
-    }
-    
-        
-    int Flip(int number)
-    {
-        
-        int result = 0;
-        
-        while(number)
-        {
-            result *= 10;    
-            result += number % 10;
-            number /= 10;  
-        }
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+float Pow(int exponent);
+long long Flip(int number);
+void Swap(int *var1, int *var2);
+void TestPow(int exponent);
+void TestFlip(int flop, int flip);
+void PowTest();
+void FlipTest();
+void SwapTest();
+void HelloWorldHex();
 
-        return result;
-        
-    }
-        
+
+int main()
+{
+
+HelloWorldHex();
+
+PowTest();
+
+SwapTest();
+
+
+return 0;
+}
+
+
+float Pow(int exponent)
+{     
+    float base = 10.0;
+    float result = 1.0;
     
-    void Swap(int *var1, int *var2)
+    if(exponent < 0)
     {
-        
-        int *tmp = &var1;
-        *var1 = &var2;
-        *var2 = &tmp;
-        return;
-           
+        base = 1.0 / base;
+        exponent = -exponent;
     }
         
-    void TestPow(int exponent)
+    for(int i = 0; i < exponent; i++)
     {
-        
-        if((double)pow(10.0, exponent) == (double)Pow(exponent))
-        {
-            printf("Great Success!!");
-        }
-        
-        else
-        {
-            printf("Great Fail!!");
-        }
-        
+        result *= base;
     }
     
-    void TestFlip(int flop, int flip)
+    return result;
+}
+
+    
+long long Flip(int number)
+{        
+    int result = 0;
+    
+    while (number != 0)
     {
-        if(flip == Flip(flop))
-        {
-            printf("Great Success!!");
-        }
-        
-        else
-        {
-            printf("Great Fail!!");
-        }
-        
-        return;
+        result *= 10;    
+        result += number % 10;
+        number /= 10;  
     }
-          
+
+    return result;        
+}
+    
+
+void Swap(int *var1, int *var2)
+{       
+    int tmp = *var1;
+    *var1 = *var2;
+    *var2 = tmp;     
+}
+    
+void TestPow(int exponent)
+{               
+    assert((float)pow(10.0, exponent) <= Pow(exponent) * 1.01 || //
+           (float)pow(10.0, exponent) >= Pow(exponent) * 0.99);
+}
+
+
+void PowTest()
+{
+    int tester[5] = {-2, -1, 0, 1, 2};
+    
+    for (int i = 0; i < 5; i++)
+    {
+        TestPow(*tester);
+        *tester += 4;
+    }
+    
+}
+
            
+void TestFlip(int flop, int flip)
+{
+    assert(flip == Flip(flop));
+}
+
+void FlipTest()
+{
+    int tester[5] = {0, 1234321, 10000, 1009002, 64};
+    int flipped[5] = {0, 1234321, 1, 2009001, 46};
+    
+    for (int i = 0; i < 5; i++)
+    {
+        TestFlip(*tester, *flipped);
+        *tester += 4;
+        *flipped += 4;
+    }    
+}
+
+void TestSwap(int *var1, int*var2)
+{
+    int *tmp1 = var1;
+    int *tmp2 = var2;
+    
+    Swap(var1, var2);
+    
+    assert(tmp1 == var2 && tmp2 == var1);
+}
+
+void SwapTest()
+{
+    int tester1 = 2, tester2 = 3;
+    TestSwap(&tester1, &tester2);
+    TestSwap(&tester1, &tester1);    
+}
+    
+
+void HelloWorldHex()
+{
+    printf("%c%c%c%c%c%c %c%c%c%c%c%c%c", 0x27, 0x48, 0x65, 0x6C, 0x6C, 0x6F, //
+                                          0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21, //
+                                          0x27);
+}      
