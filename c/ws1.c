ecc020deb0e5e3cf20f1b53407fc7d4b245dc2ff
dor.tambour <dortam888@gmail.com>
2019-07-07 16:06:44 +0300

    modified:   ws1.c 	deleted:    WS1.c 	modified:   WS2.c

--- c/ws1.c
+++ c/ws1.c
@@ -2,42 +2,45 @@
 #include <math.h>
 #include <assert.h>
 float Pow(int exponent);
-long long Flip(int number);
+int Flip(int number);
 void Swap(int *var1, int *var2);
 void TestPow(int exponent);
 void TestFlip(int flop, int flip);
+void TestSwap(int *var1, int *var2);
 void PowTest();
 void FlipTest();
 void SwapTest();
 void HelloWorldHex();
 
+#define BASE 10
 
 int main()
 {
+    HelloWorldHex();
 
-HelloWorldHex();
+    PowTest();
+    
+    FlipTest();
 
-PowTest();
+    SwapTest();
 
-SwapTest();
-
-
-return 0;
+    return 0;
 }
 
 
 float Pow(int exponent)
 {     
-    float base = 10.0;
+    float base = BASE;
     float result = 1.0;
+    int i = 0;
     
-    if(exponent < 0)
+    if (exponent < 0)
     {
         base = 1.0 / base;
         exponent = -exponent;
     }
         
-    for(int i = 0; i < exponent; i++)
+    for (i = 0; i < exponent; i++)
     {
         result *= base;
     }
@@ -45,8 +48,8 @@ float Pow(int exponent)
     return result;
 }
 
-    
-long long Flip(int number)
+
+int Flip(int number)
 {        
     int result = 0;
     
@@ -67,6 +70,7 @@ void Swap(int *var1, int *var2)
     *var1 = *var2;
     *var2 = tmp;     
 }
+   
     
 void TestPow(int exponent)
 {               
@@ -78,8 +82,9 @@ void TestPow(int exponent)
 void PowTest()
 {
     int tester[5] = {-2, -1, 0, 1, 2};
+    int i = 0;
     
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
         TestPow(*tester);
         *tester += 4;
@@ -93,18 +98,21 @@ void TestFlip(int flop, int flip)
     assert(flip == Flip(flop));
 }
 
+
 void FlipTest()
 {
     int tester[5] = {0, 1234321, 10000, 1009002, 64};
     int flipped[5] = {0, 1234321, 1, 2009001, 46};
+    int i = 0;
     
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
         TestFlip(*tester, *flipped);
         *tester += 4;
         *flipped += 4;
     }    
 }
+
 
 void TestSwap(int *var1, int*var2)
 {
@@ -116,6 +124,7 @@ void TestSwap(int *var1, int*var2)
     assert(tmp1 == var2 && tmp2 == var1);
 }
 
+
 void SwapTest()
 {
     int tester1 = 2, tester2 = 3;
@@ -126,7 +135,7 @@ void SwapTest()
 
 void HelloWorldHex()
 {
-    printf("%c%c%c%c%c%c %c%c%c%c%c%c%c", 0x27, 0x48, 0x65, 0x6C, 0x6C, 0x6F, //
-                                          0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21, //
-                                          0x27);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 0x27, 0x48, 0x65, 0x6C, 0x6C, 0x6F, //
+                                           0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, // 
+                                           0x21, 0x27);
 }      
