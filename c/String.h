ed8181915aec723af9d0aa6d9ffa422cd00895a5
dor.tambour <dortam888@gmail.com>
2019-07-09 01:30:19 +0300

    String.c

--- c/String.h
+++ c/String.h
@@ -1,9 +1,25 @@
+/* This is a header for reimplementation of some of string.h functions */
+
 size_t My_strlen(const char *str);
 
 int My_strcmp(const char *str1, const char *str2);
 
 char *My_strcpy(char *destination, const char *source);
 
-char *My_strncpy(char *destination, const char *source, size_t num );
+char *My_strncpy(char *destination, const char *source, size_t num);
 
-int strcasecmp(const char *f1, const char *f2);
+int My_strcasecmp(const char *f1, const char *f2);
+
+char *My_strchr(const char *str, int c);
+
+char *My_strdup(const char *str);
+
+char *My_strcat(char *destination, const char *source);
+
+char *My_strncat(char *destination, const char *source, size_t num);
+
+char *My_strstr(const char *haystack, const char *needle);
+
+size_t My_strspn(const char *str1, const char *str2);
+
+char *My_strtok(char *str, const char *delim);
