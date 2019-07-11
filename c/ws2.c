1d34140a0a3366286b57acf07c5b7ec7ad49cb0d
dor.tambour <dortam888@gmail.com>
2019-07-11 20:40:49 +0300

    change

--- c/ws2.c
+++ c/ws2.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> 
+#include <string.h> /* strlen, strtok */
+#include <math.h> /* abs */
+#include <stdlib.h> /*atoi */
 
 int IsPalindrome(const char *str);
 void SevenBoom(int from, int to);
 int CheckSeven(int number);
 void RmSpaces(char *str);
-
+char *StringAdd(char *str1, char *str2);
+void AddChars(char *str1, char *str2, char *destination, int *residual);
 
 int main()
 {
     
-    RmSpaces("    h as       i am    too ");
+	char *str1 = "11111111";
+	char *str2 = "222222222";
+    char *dest = StringAdd(str1, str2);
+	printf("%s", dest);
     return 0;
 }
 
@@ -44,7 +50,7 @@ void SevenBoom(int from, int to)
     while (from <= to)
     {
         
-        if (from % 7 == 0 || CheckSeven(from) == 1)
+        if (0 == from % 7  || 1 == CheckSeven(from))
         {
             printf("BOOM ");
         }
@@ -59,9 +65,9 @@ void SevenBoom(int from, int to)
 
 int CheckSeven(int num)
 {
-    while (num != 0)
+    while (0 != num)
     {
-        if (num % 10 == 7)
+        if (7 == abs(num % 10)) 
         {
             return 1;
         } 
@@ -71,7 +77,8 @@ int CheckSeven(int num)
     
     return 0;
 }
-        
+
+/*        
 void RmSpaces(char *str)
 {
     const char *s = "a";
@@ -83,24 +90,97 @@ void RmSpaces(char *str)
         token = strtok(NULL,s);
     }
 }       
-     
+*/     
 
 char *StringAdd(char *str1, char *str2)
 {
-    int remain = 0;
-    int result = 0;
-    
-    while ('\0' != str1 && '\0' != str2)
+	int result = 0;
+    int residual = 0;
+	int str1_len = strlen(str1);
+	int str2_len = strlen(str2);
+	int max_len = (str1_len > str2_len) ? str1_len : str2_len;
+	int min_len = (max_len == str1_len) ? str2_len : str1_len;
+	char *strmax = (max_len == str1_len) ? str1 : str2;
+	char *dest_start = (char *)malloc(max_len + 1);
+	char *strmin = (strmax == str1)? str2 : str1;
+	char *strmax_end = strmax + max_len - 1;
+	char *strmin_end = strmin + min_len - 1;
+	char *dest_end = dest_start + max_len; 	
+   
+    do
     {
-        result = atoi(*str1) + atoi(*str2) + remain;
-        remain = 0;
-        if (result > 10)
+		if (strmin != strmin_end)
+		{
+		    result = (*strmax_end - 48) + (*strmin_end - 48) + residual; /* -48 to turn char to int */
+
+		    if (10 <= result)
+		    {
+		        result -= 10;
+				residual = 1;
+		    }
+			else
+			{
+				residual = 0;
+			}
+
+		    sprintf(dest_end, "%d", result);
+		    strmax_end--;
+		    strmin_end--;
+			dest_end--;
+		}
+
+		else
+		{
+		    result = (*strmax_end - 48) + residual; /* -48 to turn char to int */
+
+		    if (10 <= result)
+		    {
+		        result -= 10;
+				residual = 1;
+		    }
+			else
+			{
+				residual = 0;
+			}
+
+		    sprintf(dest_end, "%d", result);
+		    strmax_end--;
+			dest_end--;
+		}
+    }
+	while (strmax_end >= strmax);
+
+	if (residual == 0)
+	{				
+		*dest_end = '0'; /* patch of patches */
+	} 
+	else
+	{
+		sprintf(dest_end, "%d", 1);	
+	}		
+		
+    return dest_start;
+}       
+
+
+void AddChars(char *str1, char *str2, char *destination, int *residual)
+{		
+		int result = 0;
+
+        result = (*str1 - 48) + (*str2 - 48) + *residual; /* -48 to turn char to int */
+
+        if (10 <= result)
         {
             result -= 10;
+			*residual = 1;
         }
-        *str1 = result;
-        str1++;
-        str2++;
-    }
-    
-        
+		else
+		{
+			*residual = 0;
+		}
+
+        sprintf(destination, "%d", result);
+        str1--;
+        str2--;
+		destination--;
+}
