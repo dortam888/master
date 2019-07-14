e483e7df40a00e5e9ab163fc3441845b89bfaf61
dor.tambour <dortam888@gmail.com>
2019-07-14 19:48:16 +0300

    new file:   ws3.c

---
+++ c/ws3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int jos(int n) 
+{
+	int soldier_circle[] = malloc(n*sizeof(int));
+	int *sword = soldiers_circle;
+	int *person = NULL;
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		soldier_circle[i] = 1;
+	}
+	
+	while (sword != person)
+	{
+		if (soldier_circle[i] == 0)
+		{
+			++sword;
+		}
+		else
+		{
+			sword += 2;	
+	
+}
+
+int main(int argc, char *argv[], char **env)
+{	
+
+	/*
+	char **Buffer = malloc(sizeof(char *));
+	char str[] = ;
+	int i = 0;
+
+	while (*env != 0)
+	{
+		*Buffer = *env;
+		*env++;
+	}
+	
+	while (str != 0)
+	{	
+		str[i] = tolower(**Buffer);
+		i++;
+	}
+
+	printf("%s", str);
+		 
+	return 0;
+	*/
+}
