be0e2cede814d57f830a0b7a2227b5740ed232ee
dor.tambour <dortam888@gmail.com>
2019-07-23 02:23:03 +0300

    modified:   ws6.c

--- c/ws6.c
+++ c/ws6.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <assert.h>
 
-double pow2(unsigned int x, int y);
+double Pow2(unsigned int x, int y);
+static void Pow2Test();
 unsigned int IsPower2(unsigned int x);
 unsigned int IsPower2Bit(unsigned int x);
-unsigned int Add1Bit(unsigned int x);
+static void IsPower2Test_A(unsigned int (*function)(unsigned int));
+static void IsPower2Test();
+unsigned int Add1(unsigned int x);
+static void Add1BitTest();
 void Exactly3Bits(unsigned int *x, unsigned int length);
+unsigned int ByteMirror(unsigned int number);
+static void ByteMirrorTest();
 unsigned int ClosestNumber(unsigned int number);
 unsigned int CountSetBits(unsigned int num);
 unsigned int CountSetBits_NoLoop(unsigned int num);
@@ -12,16 +19,33 @@ void PrintFloat(float num);
 
 int main()
 {
-	printf("%u\n", IsPower2(2048));
+	Pow2Test();
+	
+	IsPower2Test();
+	
+	Add1BitTest();
+	
+	ByteMirrorTest();
+	
 	return 0;
 }
 
 
-double pow2(unsigned int x, int y)
+double Pow2(unsigned int x, int y)
 {
-	return y > 0? x << y : x >> (-y);
+	return (y > 0)? x << y : x >> (-y);
 }
 
+
+static void Pow2Test()
+{
+	assert(Pow2(0,88) == 0);
+	assert(Pow2(999,0) == 999);
+	assert(Pow2(5,1) == 10);
+	assert(Pow2(4,3) == 32);
+	assert(Pow2(4,-1) == 2);
+	assert(Pow2(32,-5) == 1);
+}
 
 unsigned int IsPower2(unsigned int x)
 {
@@ -42,36 +66,51 @@ unsigned int IsPower2Bit(unsigned int x)
 }
 
 
-unsigned int Add1Bit(unsigned int x)
+static void IsPower2Test_A(unsigned int (*function)(unsigned int))
+{
+	assert(!function(0));
+	assert(!function(77));
+	assert(!function(78));
+	assert(function(2));
+	assert(function(2048));
+}
+
+
+static void IsPower2Test()
+{
+	IsPower2Test_A(IsPower2);
+	IsPower2Test_A(IsPower2Bit);
+}
+
+unsigned int Add1(unsigned int x)
 {
 	return -(~x);
+}
+
+
+static void Add1BitTest()
+{
+	assert(Add1(99) == 100);
+	assert(Add1(1) == 2);
+	assert(Add1(0) == 1);
 }
 
 
 void Exactly3Bits(unsigned int *x, unsigned int length)
 {
 	unsigned int i = 0;
-	unsigned int bit = 1;
 	unsigned int count = 0;
 	
 	for (; i < length; ++i)
 	{
-		while (*x > bit)
+		count = CountSetBits(*x);
+		
+		if (count == 3)
 		{
-			if ((*x & bit) == bit)
-			{
-				count++;
-			}
-			
-			bit = bit << 1;		
+			printf("%u\n", *x);
 		}
 		
-		if (3 == count)
-		{
-			printf("%d\n", *x);
-		}
-	
-		x++;
+		++x;
 	}		
 }
 
@@ -80,36 +119,48 @@ unsigned int ByteMirror(unsigned int number)
 {
 	unsigned int mirror_number = 0;
 
-	while(0 != number)
+	while(0 < number)
 	{
-		mirror_number += number & 1;
+		mirror_number |= number & 1;
 		mirror_number <<= 1;
 		number >>= 1;
 	}
 
-	return mirror_number;
+	return mirror_number >> 1;
+}
+
+static void ByteMirrorTest()
+{
+	assert(ByteMirror(9) == 9);
+	assert(ByteMirror(8) == 1);
+	assert(ByteMirror(10) == 5);
 }
 
 
-unsigned int Function1(unsigned char x)
+unsigned int Function1(unsigned int x, unsigned int pos1, unsigned int pos2)
 {
-	return ((x >> 2) & (x >> 6) & 1);
+	return ((x >> pos1) & (x >> pos2) & 1);
 }
 
 
-unsigned int Function2(unsigned int x)
+unsigned int Function2(unsigned int x, unsigned int pos1, unsigned int pos2)
 {
-	return ((x >> 2) | (x >> 6) & 1);
+	return (((x >> pos1) | (x >> pos2)) & 1);
 }
 
 
-unsigned int Function3(unsigned int x)
+unsigned int Function3(unsigned int x, unsigned int pos1, unsigned int pos2)
 {
+	unsigned int bit1 = x >> pos1;
+	unsigned int bit2 = x >> pos2;
 	
+	unsigned int xor = bit1 ^ bit2;
+	xor = (xor << pos1) | (xor << pos2);
+	return x^xor;
 }
 
 
-unsigned int ClosestNumber(unsigned int num)
+unsigned int ClosestNumberDivided16(unsigned int num)
 {
 	if (num < 16)
 	{
@@ -131,9 +182,9 @@ void XorSwap(unsigned int *num1, unsigned int *num2)
 		return;
 	}
 
-	*num1 = *num1 ^ *num2;
-	*num2 = *num1 ^ *num2;
-	*num1 = *num1 ^ *num2;	
+	*num1 ^= *num2;
+	*num2 ^= *num1;
+	*num1 ^= *num2;	
 }
 
 
@@ -143,10 +194,7 @@ unsigned int CountSetBits(unsigned int num)
 
 	while (0 != num)
 	{
-		if (1 == (num & 1))
-		{
-			++count;
-		}
+		count += (num & 1);
 		
 		num >>= 1;
 	}
@@ -158,6 +206,7 @@ unsigned int CountSetBits(unsigned int num)
 void PrintFloat(float num)
 {
 	unsigned numint = (unsigned)num;
+	
 	while (0 != numint)
 	{
 		printf("%u", (numint & 1));
