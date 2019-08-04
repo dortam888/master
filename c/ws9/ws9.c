fd7cf393d1d7364b4c3048af97a55720be45a1d5
dor.tambour <dortam888@gmail.com>
2019-08-04 04:32:56 +0300

    modified:   ws9.c

--- c/ws9/ws9.c
+++ c/ws9/ws9.c
@@ -3,15 +3,19 @@
 #include <stdlib.h> /* exit */
 #include <assert.h> /* assert */
 #include <stddef.h> /* size_t ptrdiff_t*/
+#include <limits.h> /* INT_MAX */
 
 #define WORDSIZE (sizeof(size_t))
 #define BITS_IN_BYTE 8
 #define NUMBER_OF_CHARS 256
 #define MAX(a,b) ((a) > (b))? ((a) : (b))
 #define MAX3(a,b,c) ((a) > (b))? (((a) > (c)? (a) : (c)) : ((b) > (c)? (b) : (c)))
-#define CHECKENDIANESS ((short)"\x01" == (short)1)? printf("is little endian") : printf("is big endian")
+#define CHECKENDIANESS ((short)"\x01\0" == (short)1)? printf("is little endian") : printf("is big endian")
+#define SPACE ' '
+#define TAB '\t'
 
 
+enum {NEGATIVE = -1, POSITIVE = 1};
 typedef enum Overlap {NO_OVERLAP, RIGHT_OVERLAP, LEFT_OVERLAP} overlap_t;
 
 static void *My_memcpy_ByteByByte(void *dest, const void *src, 
@@ -23,13 +27,20 @@ int My_atoibase(const char *str, int base);
 char *My_itoabase(int number, int base, char *result);
 void ThreeArrays(char *str1, char *str2, char *str3);
 void CheckEndianess();
+void Test_ThreeArrays();
+void Test_My_atoibase();
+void Test_My_itoabase();
 
 
 int main()
 {
 	Test_My_memcpy();
 	Test_My_memset();
-	CHECKENDIANESS;
+	Test_My_atoibase();
+	Test_My_itoabase();
+	Test_ThreeArrays();
+	CheckEndianess();
+	/*CHECKENDIANESS;*/
 	return 0;
 }
 
@@ -229,13 +240,13 @@ void *My_memmove(void *dest, const void *src, size_t number_of_bits)
 int My_atoi10(const char *str)
 {
 	int result = 0;
-	int sign = 1;
+	int sign = POSITIVE;
 
 	assert(NULL != str);
 
 	if ('-' == *str)
 	{
-		sign = -1;
+		sign = NEGATIVE;
 		++str;
 	}
 
@@ -256,12 +267,25 @@ int My_atoi10(const char *str)
 int My_atoibase(const char *str, int base)
 {
 	int result = 0;
+	int sign = POSITIVE;
 
 	assert(NULL != str);
+	
+	if ('-' == *str)
+	{
+		sign = NEGATIVE;
+		++str;
+	}
 
 	while ('\0' != *str)
 	{
-		if (*str - '0' < '\0')	
+		if (*str == SPACE || *str == TAB)
+		{
+			++str;
+			continue;
+		}
+		
+		if (*str - '0' < '\0' || ((*str - '0' >= base) && (*str < 'A')))	
 		{
 			break;
 		}
@@ -272,16 +296,17 @@ int My_atoibase(const char *str, int base)
 		}
 		else
 		{
-			if (*str - '0' - 6 > base || *str - '0' - 6 < '\0')
+			if (*str - '0' - 6 > base)
 			{
 				break;
 			}
 			result = result * base + *str - '0' - 7;
 		}
+		
 		++str;
 	}
 
-	return result;
+	return result*sign;
 }
 
 size_t NumberOfDigitsInNum(int num, int base)
@@ -331,6 +356,12 @@ char *My_itoabase(int number, int base, char *result)
 	char *start_of_result = result;
 	size_t num_of_digits_in_number = NumberOfDigitsInNum(number, base);
 
+	if (number == 0)
+	{
+		*result = '0';
+		++result; 
+	}
+	
 	if (number < 0)
 	{
 		*result = '-';
@@ -352,6 +383,7 @@ char *My_itoabase(int number, int base, char *result)
 		{
 			*result = number % base + '0' + 7;
 		}
+		
 		--result;
 		number /= base;
 	}
@@ -405,11 +437,11 @@ void CheckEndianess()
 	
 	if (1 == *ptr_to_number)
 	{
-		printf("system is little endian");
+		printf("system is little endian\n");
 	} 
 	else
 	{
-		printf("system is not little endian");
+		printf("system is not little endian\n");
 	}
 }
 
@@ -458,4 +490,69 @@ static void Test_My_memset()
 	memset(dest4, 5, 20);
 	
 	assert(*dest3 == *dest4 && *dest3 + 19 == *dest4 + 19);
+}
+
+void Test_ThreeArrays()
+{
+	char *str1 = "abcd ety nutcv";
+	char *str2 = "adtp lsq vv";
+	char *str3 = "ab ui";
+	char *str4 = "dtv ";
+	
+	ThreeArrays(str1, str2, str3);
+	printf("letters that should be printed: %s\n", str4);	
+}
+
+void Test_My_atoibase()
+{
+	char *tests[] = {"100", "   680		", "2147483647", "- 1 1", "ABF"};
+	int expected[] = {100, 680, 2147483647, -11, 0};
+	int expected7[] = {49, 6, 109, -8, 0};
+	size_t number_of_tests = sizeof(tests)/sizeof(tests[0]);
+	size_t i = 0, j = 0;
+	
+	for (i = 0; i < number_of_tests; ++i)
+	{
+		if (i == 0)
+		{
+			for (j = 2; j <= 36; ++j)
+			{
+				assert(My_atoibase(tests[i], j) == (int)(j*j));
+			}
+		}
+		
+		assert(My_atoibase(tests[i], 10) == expected[i]);
+		assert(My_atoibase(tests[i], 7) == expected7[i]);
+	}
+	
+	assert(My_atoibase(tests[4], 16) == 2751);
+}
+
+
+void Test_My_itoabase()
+{
+	int tests[] = {0, 1101, -25, INT_MAX};
+	char *expected[] = {"0", "1101", "-25", "2147483647"};
+	char *expected5[] = {"0", "13401", "-100", "13344223434042"};
+	char *expected16[] = {"0", "44D", "-19", "7FFFFFFF"};
+	char *expected36[] = {"0", "UL", "-P", "ZIK0ZJ"}; 
+	char result[30] = {0};
+	size_t number_of_tests = sizeof(tests)/sizeof(tests[0]);
+	size_t i = 0;
+	
+	for (i = 0; i< number_of_tests; ++i)
+	{
+		My_itoabase(tests[i], 10, result);
+		assert(0 == strcmp(result, expected[i]));
+		memset(result, 0, 30);
+		My_itoabase(tests[i], 5, result);
+		assert(0 == strcmp(result, expected5[i]));
+		memset(result, 0, 30);
+		My_itoabase(tests[i], 16, result);
+		assert(0 == strcmp(result, expected16[i]));
+		memset(result, 0, 30);
+		My_itoabase(tests[i], 36, result);
+		assert(0 == strcmp(result, expected36[i]));
+		memset(result, 0, 30);
+	}	
 }
