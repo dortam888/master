79bf236abb1bf51022206ffe7df2d8a4fb63fc8f
nitzan izhaki <missnitz@gmail.com>
2019-08-13 20:50:45 +0300

    modified:   include/linked_list.h 	modified:   linked_list.c

--- ds/linked_list/include/linked_list.h
+++ ds/linked_list/include/linked_list.h
@@ -2,88 +2,158 @@
 #define ILRD_LINKED_LIST_H
 #include <stddef.h> /* size_t */
 
+
+
 /*############################################################################*/
 /* Owner: OL72 */
 /* Reviewer: OL72 */
-/* Create: Tuesday August 13 2019 01:31:06 PM */
+/* Create: שלישי אוגוסט 13 2019 01:38:08 pm */
 /* This file contains functions for linked_list data structure operations.*/
 /* Can be used for linked_list based algorithms */
 /*############################################################################*/
 
-typedef struct node node_t;
+typedef struct node
+					{
+						void* data;
+						struct node *next_node;
+					} slist_node_t;
 
-/*#############################################################################
-  # Description: 
-	get an intial capacity and creates a circular buffer
+/*############################################################################
+  # Description:
+	creates new node 
   # Parameters:
-	capacity: memory in bytes
+	data - pointer to data requested to be stored
   # Return Value:
-	in case of success returns a pointer to a circular buffer
-	in case of failure returns NULL
-##############################################################################*/
-node_t *LinkedListCreateNode(void *data);
+	pointer to new node
+############################################################################*/
+slist_node_t *SlistCreateNode(void *data, slist_node_t *next_node);
 
-/*#############################################################################
-  # Description: 
-	get c_buff and destroy it
+/*############################################################################
+  # Description:
+	adds new node before current node. CAUSES POINTER INVALIDATION	
   # Parameters:
-	c_buff - a pointer to c_buff 
+	head - pointer to first node of linked list
+	current node - node to add before
+	new node - node to add 
+	head 0 
   # Return Value:
-	None
-##############################################################################*/
-void LinkedListFreeAll(node_t *head);
+	pointer to address of inserted node
+############################################################################*/
+slist_node_t *SlistInsertNode(slist_node_t *current_node,
+													slist_node_t *new_node);
 
-/*#############################################################################
-  # Description: 
-	get a circular buffer and returns how much bytes it can hold
+/*############################################################################
+  # Description:
+	adds new node after current node 
   # Parameters:
-	c_buff - a pointer to c_buff 
+	current node - node to add after
+	new node - node to add
   # Return Value:
-	number of bytes the circular buffer can hold
-##############################################################################*/
-int LinkedListInsert(node_t *node);
+	pointer to address of inserted node
+############################################################################*/
+slist_node_t *SlistInsertAfterNode(slist_node_t *current_node,
+												 slist_node_t *new_node);
 
-/*#############################################################################
-  # Description: 
-	get a circular buffer and returns how much bytes can be written without lose of information
+/*############################################################################
+  # Description:
+	free all linked list
   # Parameters:
-	c_buff - a pointer to c_buff 
+	head - pointer to first node of linked list
   # Return Value:
-	number of bytes that can be written
-##############################################################################*/
-int LinkedListInsertAfter(node_t *node);
+	none
+############################################################################*/
+void SlistFreeAll(slist_node_t *head);
 
-/*#############################################################################
-  # Description: 
-	get a circular buffer and reads number of bytes from it
+/*############################################################################
+  # Description:
+	removes node from linked list. CAUSES POINTER INVALIDATION	
   # Parameters:
-	c_buff - a pointer to c_buff
-	number_of_bytes - number of bytes to read from the circular buffer
+	current_node - node to remove
   # Return Value:
-	number of bytes that we read
-	returns -1 in case of failure
-##############################################################################*/
-ssize_t CBuffRead(c_buff_t *c_buff, void *buffer, size_t number_of_bytes);
+	poitnter to address of new current node
+############################################################################*/
+slist_node_t *SlistRemove(slist_node_t *current_node);
 
-/*#############################################################################
-  # Description: 
-	get a circular buffer and writes number of bytes to it
+/*############################################################################
+  # Description:
+	remove node after current node
   # Parameters:
-	c_buff - a pointer to c_buff
-	number_of_bytes - number of bytes to write to the circular buffer
+	current node
   # Return Value:
-	number of bytes that were written to the circular buffer
-	returns -1 in case of failure
-##############################################################################*/
-ssize_t CBuffWrite(c_buff_t *c_buff, void *data ,size_t number_of_bytes);
+	poitnter to address of new current node
+############################################################################*/
+slist_node_t *SlistRemoveAfter(slist_node_t *current_node);
 
-/*#############################################################################
-  # Description: 
-	get a circular buffer and check if it's empty
+/*############################################################################
+  # Description:
+	count number of nodes in linked list
   # Parameters:
-	c_buff - a pointer to c_buff
+	head - pointer to first node of linked list
   # Return Value:
-	1 if it's empty and 0 otherwise
-##############################################################################*/
-int CBuffIsEmpty(const c_buff_t *c_buff);
+	number of nodes in linked list
+############################################################################*/
+size_t SlistCount(const slist_node_t *head);
+
+/*############################################################################
+  # Description:
+	find node that contains same data as input data
+  # Parameters:
+	head - pointer to first node of linked list
+	data - data to compare
+	(*cmp_func) - compare function 
+  # Return Value:
+	if found returns address of found node, else NULL
+############################################################################*/
+typedef int (*match_func_t)(const void *node_data, const void *user_data);
+
+slist_node_t *SlistFind(const slist_node_t *head, const void *data, 
+												match_func_t match_func);
+
+/*############################################################################
+  # Description:
+	preforms action function on each node in linked list
+  # Parameters:
+	head - pointer to first node of linked list
+	(*action_func) - action function to preform
+  # Return Value:
+	in case of success returns 0, otherwise passes the return value from the action function
+############################################################################*/
+typedef int (*action_func_t)(void *data, void *param);
+
+int SlistForEach(slist_node_t *head, action_func_t action_func, 
+											 void *param_for_action_func);
+
+/*############################################################################
+  # Description:
+	flip order of linked list
+  # Parameters:
+	head - pointer to first node of linked list
+  # Return Value:
+	poitnter to address of new head
+############################################################################*/
+slist_node_t *SlistFlip(const slist_node_t *head);
+
+/*############################################################################
+  # Description:
+	check if linked list contains loop
+  # Parameters:
+	head - pointer to first node of linked list
+  # Return Value:
+	1 if loop found, 0 otherwise
+############################################################################*/
+int SlistHasLoops(const slist_node_t *head);
+
+/*############################################################################
+  # Description:
+	check if Intersection is found
+  # Parameters:
+	head1 - pointer to first node of first linked list
+	head2 - pointer to first node of second linked list
+  # Return Value:
+	address of found intersected node. otherwise NULL
+############################################################################*/
+slist_node_t *SlistFindIntersection(const slist_node_t *head1,
+									const slist_node_t *head2);
+
+
 #endif /* ILRD_LINKED_LIST_H */
--- ds/linked_list/linked_list.c
+++ ds/linked_list/linked_list.c
@@ -1,1 +1,113 @@
 #include "linked_list.h"
+#include <assert.h> /*assert*/
+
+slist_node_t *SlistCreateNode(void *data, slist_node_t *next_node)
+{
+	slist_node_t *new_node = malloc(sizeof(slist_node_t));
+
+	if(NULL != new_node)
+	{
+		new_node->data = data;
+		new_node->next_node = next_node;
+	}
+
+	return new_node
+}
+
+static void SwapPointers(void **ptr1, void **ptr2)
+{
+	void *tmp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = tmp;
+}
+
+static void SwapNodes(slist_node_t *node1, slist_node_t *node2)
+{
+	SwapPointers(&(node1->next_node), &(node2->next_node));
+	SwapPointers(&(node1->data), &(node2->data));
+}
+
+
+slist_node_t *SlistInsertNode(slist_node_t *current_node,
+														 slist_node_t *new_node)
+{
+	assert(NULL != current_node && NULL != new_node);
+
+	new_node->next_node = current_node;
+	SwapPointers(current_node->next_node, new_node->next_node);
+
+	return new_node;
+}
+
+slist_node_t *SlistInsertAfterNode(slist_node_t *current_node,
+														 slist_node_t *new_node)
+{
+	assert(NULL != current_node && NULL != new_node);
+
+	new_node->next_node = current_node->next_node;
+	current_node->next_node = new_node;
+
+	return new_node;
+}
+
+void SlistFreeAll(slist_node_t *head)
+{
+	if (NULL != head)
+	{
+		slist_node_t *tmp = head->next_node;
+		free(head);
+		head = NULL;
+		SlistFreeAll(tmp);
+	}
+}
+
+slist_node_t *SlistRemove(slist_node_t *current_node)
+{
+	assert(NULL != current_node);
+
+}
+
+slist_node_t *SlistRemoveAfter(slist_node_t *current_node)
+{
+
+}
+
+size_t SlistCount(const slist_node_t *head)
+{
+	static size_t node_counter;
+
+	if (NULL != head)
+	{
+		slist_node_t *tmp = head->next_node;
+		++node_counter;
+		SlistCount(tmp);
+	}
+}
+
+slist_node_t *SlistFind(const slist_node_t *head, const void *data, 
+												  match_func_t match_func)
+{
+	
+}
+
+int SlistForEach(slist_node_t *head, action_func_t action_func, 
+									 void *param_for_action_func)
+{
+
+}
+
+slist_node_t *SlistFlip(const slist_node_t *head)
+{
+
+}
+
+int SlistHasLoops(const slist_node_t *head)
+{
+
+}
+
+slist_node_t *SlistFindIntersection(const slist_node_t *head1,
+									const slist_node_t *head2)
+{
+
+}
