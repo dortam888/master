67870eb7a5fea78d48f14e2a07685ddb91581596
nitzan izhaki <missnitz@gmail.com>
2019-08-18 11:38:25 +0300

    modified:   circular_buffer.c

--- ds/circular_buffer/circular_buffer.c
+++ ds/circular_buffer/circular_buffer.c
@@ -1,37 +1,54 @@
-#include "circular_buffer.h"
+/*******************************************************************************
+**** Author: Dor Tambour
+**** Last Update: 15/08/2019
+**** Reviewer: Nitzan
+**** Description: This file contains the implementations of functions
+				  for the data structure circular buffer.
+				  Look at circular_buffer.h for more information about the 
+				  functions.
+*******************************************************************************/
+
 #include <stdlib.h> /* malloc free */
 #include <string.h> /* memcpy */
 #include <assert.h> /* assert */
 
-#define INITIAL_VALUE (1)
-#define MIN(a,b) ((a) < (b)) ? (a) : (b)
+#include "circular_buffer.h" /* c_buff_t */
 
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+#define WORDSIZE sizeof(size_t)
+#define INITIAL_VALUE 1
 
-#pragma pack(1)
 struct c_buff
 {
-	void *end;
-	void *read;
-	size_t bytes_left_to_read;
-	char circular_buffer_start[INITIAL_VALUE];
+	char *end; /* fixed pointer to the end of the buffer. char * for pointer arithematic consideration*/
+	char *read; /* pointer that indicates the last byte that was read */
+	size_t written_bytes_not_read; /* how many bytes were written and weren't been read*/
+	char circular_buffer_start[INITIAL_VALUE]; /* fixed pointer to the start of the circ_buffer. */
 };
 
 c_buff_t *CBuffCreate(size_t capacity)
 {
-	c_buff_t *circ_buff = malloc(capacity + sizeof(c_buff_t) - INITIAL_VALUE);
-
-	if (NULL != circ_buff)
+	c_buff_t *circ_buff = malloc(capacity + sizeof(c_buff_t) + INITIAL_VALUE -
+								 WORDSIZE); /* save allocated memory that is added from struct padding */
+	if (NULL == circ_buff)
 	{
-		circ_buff->end = circ_buff->circular_buffer_start + capacity;
-		circ_buff->read = circ_buff->circular_buffer_start;
-		circ_buff->bytes_left_to_read = 0;
+		return NULL;
 	}
+
+	circ_buff->end = circ_buff->circular_buffer_start + capacity;
+	circ_buff->read = circ_buff->circular_buffer_start;
+	circ_buff->written_bytes_not_read = 0;
 
 	return circ_buff;
 }
 
 void CBuffDestroy(c_buff_t *c_buff)
 {
+	assert(NULL != c_buff);
+
+	c_buff->end = NULL;
+	c_buff->read = NULL;
+
 	free(c_buff);
 	c_buff = NULL;
 }
@@ -40,34 +57,41 @@ size_t CBuffCapacity(const c_buff_t *c_buff)
 {
 	assert(NULL != c_buff);
 
-	return ((char *)c_buff->end - c_buff->circular_buffer_start);
+	return (c_buff->end - c_buff->circular_buffer_start);
 }
 
 size_t CBuffFreeSpace(const c_buff_t *c_buff)
 {
 	assert(NULL != c_buff);
 
-	return CBuffCapacity(c_buff) - c_buff->bytes_left_to_read;
+	return (CBuffCapacity(c_buff) - c_buff->written_bytes_not_read);
+}
+
+static int IsEndShouldPassed(void *end, void *read_or_write, 
+							 size_t number_of_bytes_of_action)
+{
+	return !((char *)end >= (char *)read_or_write + number_of_bytes_of_action);
 }
 
 ssize_t CBuffRead(c_buff_t *c_buff, void *buffer, size_t number_of_bytes)
 {
-	int return_value = 0;
-	size_t number_of_bytes_to_read = 0;
+	size_t actual_number_of_bytes_to_read = 0;
 
-	assert(NULL != buffer && NULL != c_buff);
+	assert(NULL != buffer);
+	assert(NULL != c_buff);
 
-	/* number_of_bytes is what requested bytes_left_to_read is what i can read */
-	number_of_bytes_to_read = MIN(number_of_bytes, c_buff->bytes_left_to_read);
+	/* take the minimum from 9 */
+	actual_number_of_bytes_to_read = MIN(number_of_bytes, 
+										 c_buff->written_bytes_not_read);
 
-	c_buff->bytes_left_to_read -= number_of_bytes_to_read; /* update how much i can read */
+	c_buff->written_bytes_not_read -= actual_number_of_bytes_to_read; /* update how much i can read */
 
 	/* check if we read over end if not copy in one step if yes read in 2 steps */
-	if ((char *)c_buff->end >= 
-		(char *)c_buff->read + number_of_bytes_to_read)
+	if (!IsEndShouldPassed(c_buff->end, c_buff->read, 
+						   actual_number_of_bytes_to_read))
 	{
-		memcpy(buffer, c_buff->read, number_of_bytes_to_read);
-		c_buff->read = (char *)c_buff->read + number_of_bytes_to_read;
+		memcpy(buffer, c_buff->read, actual_number_of_bytes_to_read);
+		c_buff->read = c_buff->read + actual_number_of_bytes_to_read;
 	}
 	else
 	{
@@ -75,63 +99,61 @@ ssize_t CBuffRead(c_buff_t *c_buff, void *buffer, size_t number_of_bytes)
 							 (size_t)c_buff->read;
 		/* step 1 read till the end */
 		memcpy(buffer, c_buff->read, end_to_read); 
-		number_of_bytes_to_read -= end_to_read; /*update number of bytes to read */
+		actual_number_of_bytes_to_read -= end_to_read; /*update number of bytes to read */
 
 		/* step 2 from beginning till bytes to cpy ends */
 		memcpy((char *)buffer + end_to_read, 
-				c_buff->circular_buffer_start, number_of_bytes_to_read); 
+				c_buff->circular_buffer_start, actual_number_of_bytes_to_read); 
 		c_buff->read = c_buff->circular_buffer_start + 
-					   number_of_bytes_to_read;
+					   actual_number_of_bytes_to_read;
 	}
 
-	return_value = number_of_bytes_to_read; /* update how much bytes were read */
-
-	return return_value;
+	return actual_number_of_bytes_to_read;
 }
 
 ssize_t CBuffWrite(c_buff_t *c_buff, void *data ,size_t number_of_bytes)
 {
-	size_t number_of_bytes_to_write = 0;
-	void *write_ptr = NULL;
+	size_t actual_number_of_bytes_to_write = 0;
+	char *write_ptr = NULL; /* char * to avoid casting */
 
 	assert(NULL != c_buff && NULL != data);
 
-	number_of_bytes_to_write = MIN(number_of_bytes, CBuffFreeSpace(c_buff));
+	actual_number_of_bytes_to_write = MIN(number_of_bytes, 
+										  CBuffFreeSpace(c_buff));
 
-	
-	write_ptr = (char *)c_buff->read + c_buff->bytes_left_to_read;
+	write_ptr = c_buff->read + c_buff->written_bytes_not_read;
 
 	/* check from where we should write */
-	if ((char *)write_ptr > (char *)c_buff->end)
+	if (write_ptr > c_buff->end)
 	{
 		write_ptr = c_buff->circular_buffer_start + (size_t)c_buff->read + 
-					c_buff->bytes_left_to_read - (size_t)c_buff->end;
+					c_buff->written_bytes_not_read - (size_t)c_buff->end;
 	}
 
 	/* update how much bytes i can read after write */
-	c_buff->bytes_left_to_read += number_of_bytes_to_write;
+	c_buff->written_bytes_not_read += actual_number_of_bytes_to_write;
 
 	/* check if we should write over the end if not simply copy the data if yes copy in two steps */
-	if ((char *)c_buff->end >= (char *)write_ptr + number_of_bytes_to_write)
+	if (!IsEndShouldPassed(c_buff->end, write_ptr, 
+						   actual_number_of_bytes_to_write))
 	{
-		memcpy(write_ptr, data, number_of_bytes_to_write); /* copy data */
+		memcpy(write_ptr, data, actual_number_of_bytes_to_write); /* copy data */
 	}
 	else
 	{
-		size_t end_to_write = (size_t)c_buff->end - 
-							  (size_t)write_ptr;
-		memcpy(write_ptr, data, end_to_write); /* step one from start of write to end */
-		number_of_bytes_to_write -= end_to_write;
+		size_t end_to_write = (size_t)c_buff->end - (size_t)write_ptr;
+		memcpy(write_ptr, data, end_to_write); /* step one cpy from start of write to end */
+		actual_number_of_bytes_to_write -= end_to_write;
 		memcpy(c_buff->circular_buffer_start, (char *)data + end_to_write, 
-			   number_of_bytes_to_write); /* step two from start to how much bytes left to copy */
+			   actual_number_of_bytes_to_write); /* step two cpy from start bytes left to copy */
 	}
 
-	return number_of_bytes_to_write;
+	return actual_number_of_bytes_to_write;
 }
 
 int CBuffIsEmpty(const c_buff_t *c_buff)
 {
 	assert(NULL != c_buff);
 
-	return (!(c_buff->bytes_left_to_read));
+	return (!(c_buff->written_bytes_not_read));
 }
